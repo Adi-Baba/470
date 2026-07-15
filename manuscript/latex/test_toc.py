ad677e40842a8e4cd1c5c8280212ddef744ca71b
Aditya Kumar <aditya.kumar@example.com>
2026-07-15 23:21:38 +0530

    Ignore and remove useless LaTeX build files and python scripts

--- manuscript/latex/test_toc.py
+++
@@ -1,7 +0,0 @@
-with open('erdos_paper.cls', 'r') as f:
-    tex = f.read()
-
-tex = tex.replace('\\pagestyle{plain}', '\\pagestyle{plain}\n\\setcounter{secnumdepth}{0}\n\\setcounter{tocdepth}{3}')
-
-with open('erdos_paper.cls', 'w') as f:
-    f.write(tex)
